80742b648e4509b6d0dda81851abaf05261d62ca
ats1999 <rkt82224@gmail.com>
2021-10-27 06:57:23 +0530

    init  tui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import "@toast-ui/editor/dist/toastui-editor.css";
+import { Editor } from "@toast-ui/react-editor";
+
 function App() {
   return (
     <div className="App">
       <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <h1>App1</h1>
+      <Editor autofocus={false} />
     </div>
   );
 }
